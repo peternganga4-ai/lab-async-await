70dae097b73d0b4ad24b105f7b52d9b9f9f19de2
Peter Ng'ang'a <peter.nganga4@student.moringaschool.com>
2026-07-19 22:54:22 +0300

    implement async fetch and display posts

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your code here!
+const postList = document.getElementById("post-list");
+
+
+function displayPosts(posts) {
+  posts.forEach((post) => {
+    
+    const li = document.createElement("li");
+
+    
+    const title = document.createElement("h1");
+    title.textContent = post.title;
+
+    
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+
+    li.appendChild(title);
+    li.appendChild(body);
+
+    
+    postList.appendChild(li);
+  });
+}
+
+
+async function getPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const posts = await response.json();
+
+    displayPosts(posts);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}
+
+
+getPosts();
